532184f6923069d52fdb6c239b31d41c78e7d231
Chiel Bruin <git@bruin.ch>
2019-08-29 14:50:31 +0200

    Finalized config for the FrameVM languages

--- spoofax-pygments/lexers/frameVM_lexer.py
+++ spoofax-pygments/lexers/frameVM_lexer.py
@@ -1,24 +1,29 @@
-from pygments.token import Text, Name, Number, String, Comment, Punctuation, Other, Keyword, Operator
-
-
 class FrameVMLexer(AbstractSpoofaxLexer):
     def __init__(self, **options):
         table_location = 'fvm.tbl'
         types = {
+            # Common
             'LAYOUT': Comment,
             'INT': Number.Integer,
             'INTLEZ': Number.Integer,
             'INTLZ': Number.Integer,
             'CONTSLOT': Name.Constant,
-            'SInstr': Keyword.Reserved,
-            'SStackInstr': Keyword.Reserved,
-            'SIntInstr': Keyword.Reserved,
-            'STypeInstr': Keyword.Reserved,
-            'SContInstr': Keyword.Reserved,
-            'SClosInstr': Keyword.Reserved,
-            'SFrameInstr': Keyword.Reserved,
+            'Header': Keyword.Declaration,
+            'PsdBlock': String.Doc,
             'LABEL': Name.Label,
-            'SControlInstr': Keyword.Reserved
+            'Path': Punctuation,
+            'EmptyPath': Punctuation,
+            
+            # Stacy
+            'SInstr': Name.Builtin,
+            'SOnReturnInstr': Name.Builtin,
+            'SControlInstr': Name.Builtin,
+            
+            # Roger
+            'Exp': Name.Builtin,
+            'Stmt': Name.Builtin,
+            'ControlStmt': Name.Builtin,
+            'RGRVar': Name.Variable
         }
         arguments = ['--parseForest', 'LayoutSensitive', '--reducing', 'LayoutSensitive', '--stack', 'Basic']
         super(FrameVMLexer, self).__init__('FrameVMLexer', table_location, types, default_type=Text, arguments=arguments, **options)
