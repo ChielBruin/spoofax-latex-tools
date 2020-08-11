cf388e38a2431ca82aa3625a55b04e847516e952
Bram Crielaard <bram@crielaard.co.uk>
2020-08-11 15:55:03 +0200

    Remove python2 compatibility layer

--- spoofax-pygments/spoofax_lexer.py
+++ spoofax-pygments/spoofax_lexer.py
@@ -9,13 +9,6 @@ from pygments.formatters.latex import LatexFormatter
 from pygments.lexer import Lexer
 from pygments.style import Style, StyleMeta
 from pygments.token import Token
-# check if pygments still has a compatibility layer
-try:
-    from pygments.util import add_metaclass
-    has_deprecated_pygments = True
-except ImportError:
-    has_deprecated_pygments = False
-
 
 # Subclass of Style that we can mess with without interfering with the original implementation
 class EmptyStyle(Style):
@@ -93,13 +86,8 @@ class CustomFormatter(LatexFormatter):
 
         # This extra style class is created to trigger the __new__ method in StyleMeta, to initialize the styles
         # that have been set in the CustomLexer
-        if has_deprecated_pygments:
-            @add_metaclass(StyleMeta)
-            class ExtraStyle(EmptyStyle):
-                pass
-        else:
-            class ExtraStyle(EmptyStyle, metaclass=StyleMeta):
-                pass
+        class ExtraStyle(EmptyStyle, metaclass=StyleMeta):
+            pass
 
 
         outfile.write("{\n")
